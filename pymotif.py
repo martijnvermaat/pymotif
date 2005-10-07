fca9525bad5eeb2d364833576a3f0440c3b6557f
Martijn Vermaat <martijn@vermaat.name>
2005-10-07 23:37:01 +0000

    Changed a lot in code. Added basic framework for program. Implemented random motif position calculations. Implemented calculate_motif matrix function.

--- pymotif.py
+++ pymotif.py
@@ -58,9 +58,9 @@ def calculate_m(W, S, b):
 
             # M[i][j] = frequency of i on position j
             M[i][j] = 0
-            for s = S:
+            for s in S:
                 if s[ b[s]+j-1 ] == i: M[i][j]++
-                M[i][j] *= 1/n
+            M[i][j] *= 1/n
 
     # implementation from page 6 of lect4.pdf, slide 4
 
@@ -97,15 +97,101 @@ Martijn Vermaat mvermaat@cs.vu.nl
 
 
 VERSION = "0.1"
-DATE = "2005/10/05"
+DATE = "2005/10/08"
 
 
 import sys
+from random import randint
 from optparse import OptionParser
 from Bio import Fasta
 
 
 def main():
+
+    """
+    Main program.
+
+    Real algorithm should be in a separate gibbs() function.
+    """
+
+    sequences, motif_width = initialize()
+
+    random_motif_positions(sequences, motif_width)
+
+    motif = calculate_motif(sequences, motif_width)
+
+    # Print start of each sequence and current motif position
+    for s in sequences:
+        print "%s... motif at position %s" % (s['sequence'][:40], s['motif_position'])
+
+    # Print position weight matrix for motif
+    for base in "ATCG":
+        print base,
+        for weight in motif[base]:
+            print weight,
+        print
+
+
+def random_motif_positions(sequences, motif_width):
+
+    """
+    Populate the list of sequences with a random position of the motif for
+    each sequence.
+    """
+
+    for s in sequences:
+        s['motif_position'] = randint(0, len(s['sequence']) - motif_width)
+
+
+def calculate_motif(sequences, motif_width):
+
+    """
+    Calculate the position weight matrix for the motif.
+
+    This implementation is very naive, use pseudocounts.
+    """
+
+    # This is ugly, but just to populate the matrix
+    # All values will be reset to 0 in algorithm
+    motif = {'A': range(0, motif_width),
+             'T': range(0, motif_width),
+             'C': range(0, motif_width),
+             'G': range(0, motif_width)}
+
+    # For all bases and all positions of motif
+    for i in "ATCG":
+        for j in range(0, motif_width):
+
+            # Initialize value to 0
+            motif[i][j] = 0
+
+            # For each sequence, add 1 if it has base i at position j
+            for s in sequences:
+                position = s['motif_position'] + j - 1
+                if s['sequence'][position] == i:
+                    motif[i][j] += 1
+
+            # Divide by the number of sequences and we have the weight of base
+            # i at position j
+
+            # Hmm, this seems to truncate to 0, how do we use real numbers?
+            #motif[i][j] /= len(sequences)
+
+    return motif
+
+
+def initialize():
+
+    """
+    Parse command line options, and read input Fasta file.
+
+    Provides:
+      sequences: a list of dictionary objects having 'title', 'sequence', and
+                 'motif_position' attributes.
+      width: width of motif to find
+
+    Return these as a pair (sequences, width).
+    """
 
     parser = OptionParser(usage = "usage: %prog -i FILE [options]",
                           version = "PyMotif %s (%s)" % (VERSION, DATE),
@@ -114,8 +200,6 @@ def main():
 
     parser.add_option("-i", "--input", dest="input", metavar="FILE",
                       help="read FILE in Fasta format")
-    parser.add_option("-s", "--samples", dest="samples", action="store_true",
-                      default=False, help="print first ten bases of each gene")
     parser.add_option("-w", "--width", dest="width", metavar="WIDTH",
                       type="int", help="find motif of width WIDTH")
 
@@ -124,23 +208,27 @@ def main():
     if not options.input:
         parser.error("input file required")
 
-    # Sample data in Fasta format
+    if not options.width:
+        parser.error("width argument required")
+
+    # Read contents of Fasta file
     try:
         file = open(options.input)
     except IOError:
         parser.error("could not read file %s" % options.input)
 
-    # Parser for Fasta format
-    parser = Fasta.RecordParser()
+    fasta_parser = Fasta.RecordParser()
 
     # Iterator for sample data
-    iterator = Fasta.Iterator(file, parser)
+    fasta_iterator = Fasta.Iterator(file, fasta_parser)
 
-    for record in iterator:
-        # Available: record.title, record.sequence, record._colwidth
-        print record.title
-        if options.samples:
-            print "  (First ten bases: %s)" % record.sequence[:10]
+    # A list containing a dictionary object for each sequence
+    sequences = [{'title':          record.title,
+                  'sequence':       record.sequence,
+                  'motif_position': 0}
+                 for record in fasta_iterator]
+
+    return sequences, options.width
 
 
 if __name__ == "__main__":
